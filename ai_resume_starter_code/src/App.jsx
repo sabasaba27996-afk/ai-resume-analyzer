9ac903a48ba1e0295902f3f761872a1893441085
saba <sabasaba27996@gmail.com>
2026-09-07 00:04:08 +0530

    commit changes

--- ai_resume_starter_code/src/App.jsx
+++ ai_resume_starter_code/src/App.jsx
@@ -1,6 +1,6 @@
 function App() {
   return (
-    <div className="min-h-screen bg-gray-900 p-4 sm:p-6 lg:p-8 flex items-center justify-center">
+    <div className="min-h-screen bg-main-gradient p-4 sm:p-6 lg:p-8 flex items-center justify-center">
       <h1 className="text-7xl text-white">AI RESUME ANALYZER</h1>
     </div>
   );
