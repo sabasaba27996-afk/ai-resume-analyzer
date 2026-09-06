b7b3a54d45f0188a69904bf2ea319cd5dc41fbb8
saba <sabasaba27996@gmail.com>
2026-09-06 23:51:43 +0530

    fixed syntax error

--- ai_resume_starter_code/src/App.jsx
+++ ai_resume_starter_code/src/App.jsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <div className="min-h-screen bg-gray-900 p-4 sm:p-6 lg:p-8 flex items-center justify-center">
-      <h1 className="text-7xltext-white">AI RESUME ANALYZER</h1>
+      <h1 className="text-7xl text-white">AI RESUME ANALYZER</h1>
     </div>
   );
 }
